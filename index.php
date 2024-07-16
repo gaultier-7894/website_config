017dc78d90c9488bc587149862a3960b509c1e86
gaultier-7894 <totipaul78@gmail.com>
2024-07-16 15:03:04 +0200

    update index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Site Web</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Assurez-vous que le fichier CSS compilé est lié ici -->
+</head>
+<body>
+    <?php include 'header.php'; ?>
+
+    <main>
+        <?php include 'main.php'; ?>
+    </main>
+
+    <?php include 'footer.php'; ?>
+</body>
+</html>
